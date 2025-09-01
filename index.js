334eeb2c55bcbb74c9644069137a1e28c790b818
Alexis Thullier <thullier.alexis@hotmail.fr>
2025-09-01 11:17:02 +0200

    Middleware

--- index.js
+++ index.js
@@ -9,21 +9,45 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.end("GET : /");
-});
+const m1 = (req, res, next) => {
+  console.log("Middleware : m1");
+  next();
+};
 
-app.post("/", (req, res) => {
-  res.end("POST : /");
-});
+const m2 = (req, res, next) => {
+  console.log("Middleware : m2");
+  next();
+};
 
-app.get("/personne", (req, res) => {
-  res.end("GET : /personne");
-});
+const m3 = (req, res) => {
+  console.log("Middleware : m3");
+};
 
-app.get("/formation", (req, res) => {
-  res.end("GET : /formation");
-});
+app.get(
+  "/",
+  (req, res, next) => {
+    console.log("GET : /");
+    res.end("GET : /");
+    next();
+  }
+  // [m2, m1]
+);
+
+// app.use(m1);
+
+// app.use(m2);
+
+// app.post("/", (req, res) => {
+//   res.end("POST : /");
+// });
+
+// app.get("/personne", (req, res) => {
+//   res.end("GET : /personne");
+// });
+
+// app.get("/formation", (req, res) => {
+//   res.end("GET : /formation");
+// });
 
 const PORT = process.env.PORT || 5000;
 
